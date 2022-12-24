6102739ba3a233a4cd8d7c7747ac9cc57cf23554
Shereshkov-Yuriy <90192795+Shereshkov-Yuriy@users.noreply.github.com>
2022-12-24 22:30:05 +0300

    lesson 1 ready (#1)

--- config/app.py
+++ config/app.py
@@ -1,8 +1,31 @@
-from flask import Flask
+import re
+from datetime import datetime
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def index():
-    return "Hello, my frend!"
-    
+@app.route("/<name>")
+@app.route("/greet/<name>")
+def greet_name(name="Friend"):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = f"<h1>Hello there, {clean_name}! It's {formatted_now}</h1>"
+    return content
+
+
+@app.route("/user/")
+def reed_user():
+    user = request.args.get("name")
+    headers = request.headers
+    return f"Hello, {user}!\n{headers}"
