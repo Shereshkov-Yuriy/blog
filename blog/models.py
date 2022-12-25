b962c4788c45d756aa0e55c3edf1679527bf7e77
Shereshkov Yuriy <yuriy.shereshkoff@gmail.com>
2022-12-25 21:14:14 +0300

    lesson 4 ready

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class User(db.Model, UserMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(255))
